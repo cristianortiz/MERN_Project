277f2907a54b6ebf2a79ba4054c7a0deb8291363
cristian <cristian.ortiz@uda.cl>
2021-05-06 23:24:44 -0400

    implementing update the state of a task and delete of task, END of backend CRUD operations.. for now

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -1,7 +1,6 @@
 const Tasks = require("../models/Tasks");
-const Project = require("../models/Projects");
-const { validationResult } = require("express-validator");
 const Projects = require("../models/Projects");
+const { validationResult } = require("express-validator");
 
 //create a nre task associed whit a project id
 exports.createTask = async (req, res) => {
@@ -11,15 +10,14 @@ exports.createTask = async (req, res) => {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  //extracting project_id from new task  data
-  const { project_id } = req.body;
   try {
+    //extracting project_id from new task  data
+    const { project_id } = req.body;
     //check if the project associated whit the new task already exists
     const project = await Projects.findById(project_id);
     if (!project) {
       return res.status(404).json({ msg: "project doesn't exists" });
     }
-
     //check if the creator of the project is currently logged
     if (project.creator.toString() !== req.user.id) {
       return res.status(401).json({ msg: "permission denied" });
@@ -29,6 +27,94 @@ exports.createTask = async (req, res) => {
     const task = new Tasks(req.body);
     await task.save();
     res.json({ task });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurs");
+  }
+};
+
+//get tasks by project id
+exports.getTasks = async (req, res) => {
+  try {
+    //extracting project_id from req  data
+    const { project_id } = req.body;
+    //check if the project associated whit the tasks already exists
+    const project = await Projects.findById(project_id);
+    if (!project) {
+      return res.status(404).json({ msg: "project doesn't exists" });
+    }
+    //check if the creator of the project is currently logged
+    if (project.creator.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "permission denied" });
+    }
+
+    //get task list of a project
+    const tasks = await Tasks.find({ project_id });
+    res.json({ tasks });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurs");
+  }
+};
+//update the state of a task
+exports.updateTask = async (req, res) => {
+  try {
+    //extracting project_id from req  data
+    const { task_name, state, project_id } = req.body;
+    //check if the task exists id came from the req data '/:id'
+    const task_exists = await Tasks.findById(req.params.id);
+    if (!task_exists) {
+      return res.status(404).json({ msg: "Task doesn't exists" });
+    }
+    //extracts project to check if their owner is logged
+    const project = await Projects.findById(project_id);
+    if (!project) {
+      return res.status(404).json({ msg: "project doesn't exists" });
+    }
+    //check if the creator of the project is currently logged
+    if (project.creator.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "permission denied" });
+    }
+
+    //after passing the previous validations creates the new task object
+    const newTask = {};
+    if (task_name) newTask.task_name = task_name;
+    if (state) newTask.state = state;
+
+    //save the task object
+    const task = await Tasks.findOneAndUpdate({ _id: req.params.id }, newTask, {
+      new: true,
+    });
+    res.json({ task });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurs");
+  }
+};
+
+//delete a task by id
+exports.deleteTask = async (req, res) => {
+  try {
+    //extracting project_id from req  data
+    const { project_id } = req.body;
+    //check if the task exists id came from the req data '/:id'
+    const task_exists = await Tasks.findById(req.params.id);
+    if (!task_exists) {
+      return res.status(404).json({ msg: "Task doesn't exists" });
+    }
+    //extracts project to check if their owner is logged
+    const project = await Projects.findById(project_id);
+    if (!project) {
+      return res.status(404).json({ msg: "project doesn't exists" });
+    }
+    //check if the creator of the project is currently logged
+    if (project.creator.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "permission denied" });
+    }
+
+    //after the previous validations delete the task
+    await Tasks.findOneAndRemove({ _id: req.params.id });
+    res.json({ msg: "Task removed" });
   } catch (error) {
     console.log(error);
     res.status(500).send("An error occurs");
--- server/routes/tasksRoutes.js
+++ server/routes/tasksRoutes.js
@@ -17,4 +17,13 @@ router.post(
   [check("project_id", "a task must be associated whit a project").notEmpty()],
   createTask
 );
+//get tasks by project id, this method do not need validation
+router.get("/", auth, getTasks);
+
+//update a task by id, change its state into 'complete' or 'pending'
+router.put("/:id", auth, updateTask);
+
+//delete a task by id
+router.delete("/:id", auth, deleteTask);
+
 module.exports = router;
